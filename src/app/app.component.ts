ef1e41e5a26359f16d3722431a79c7ba921382b7
Matias Palaversich <mep@dev.com>
2025-02-18 20:38:28 -0300

    feature: primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'refrigon-service';
+
+
+  mensajeWhatApp?: string = environment.mensajeWhatApp;
+  numberCelular?: string = environment.numberCelular;
+  numberFijo?: string = environment.numberFijo;
+  email?: string = environment.email;
+;
+
+
+clickRedirect(url: string) {
+    window.open(url, '_blank');
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {   
+    numberCelular: '5491148633866',
+    numberFijo: '4861-2469',
+    mensajeWhatApp: 'Hola, necesito realizar una consulta y quisiera asesoramiento de un técnico especializado.',
+    email: 'refrigonservice@gmail.com',
+    diasSemana: {
+        dias: 'Lunes|Viernes',
+        horario: '10:00 a 18:00'
+    },        
+    diaSabado: {
+        enable: true,
+        horario: '10:00 a 14:00'
+    }
+};
+
