36765ff99702141397f98ff28e38f0159455f971
Matias Palaversich <mep@dev.com>
2025-03-07 21:25:08 -0300

    feature: cambiosd de wording

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
-export const environment = {   
+export const environment = {
     numberCelular: '5491148633866',
-    numberFijo: '4861-2469',
+    numberFijo: '4444-2222',
     mensajeWhatApp: 'Hola, necesito realizar una consulta y quisiera asesoramiento de un técnico especializado.',
-    email: 'refrigonservice@gmail.com',
+    email: 'mep.devcode@gmail.com',
     diasSemana: {
         dias: 'Lunes|Viernes',
         horario: '10:00 a 18:00'
-    },        
+    },
     diaSabado: {
         enable: true,
         horario: '10:00 a 14:00'
